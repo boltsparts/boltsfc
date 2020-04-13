1995e11a308ea5e0c0ad0b9a3b41064fa4d26448
Bernd Hahnebach <bernd@bimstatik.org>
2020-04-13 23:26:12 +0200

    update to BOLTS commit b8d27e4: freecad backend: add new upns to FreeCAD backend

--- BOLTS/gui/tableindex_widget.py
+++ BOLTS/gui/tableindex_widget.py
@@ -2,43 +2,30 @@
 
 # Form implementation generated from reading ui file '/home/hugo/Documents/dev/bolts/BOLTS/output/freecad/BOLTS/gui/tableindex_widget.ui'
 #
-# Created by: PyQt4 UI code generator 4.12.1
+# Created by: PyQt5 UI code generator 5.11.3
 #
 # WARNING! All changes made in this file will be lost!
 
-from PyQt4 import QtCore, QtGui
-
-try:
-    _fromUtf8 = QtCore.QString.fromUtf8
-except AttributeError:
-    def _fromUtf8(s):
-        return s
-
-try:
-    _encoding = QtGui.QApplication.UnicodeUTF8
-    def _translate(context, text, disambig):
-        return QtGui.QApplication.translate(context, text, disambig, _encoding)
-except AttributeError:
-    def _translate(context, text, disambig):
-        return QtGui.QApplication.translate(context, text, disambig)
+from PyQt5 import QtCore, QtGui, QtWidgets
 
 class Ui_TableIndexWidget(object):
     def setupUi(self, TableIndexWidget):
-        TableIndexWidget.setObjectName(_fromUtf8("TableIndexWidget"))
+        TableIndexWidget.setObjectName("TableIndexWidget")
         TableIndexWidget.resize(207, 68)
-        self.horizontalLayout = QtGui.QHBoxLayout(TableIndexWidget)
-        self.horizontalLayout.setObjectName(_fromUtf8("horizontalLayout"))
-        self.label = QtGui.QLabel(TableIndexWidget)
-        self.label.setObjectName(_fromUtf8("label"))
+        self.horizontalLayout = QtWidgets.QHBoxLayout(TableIndexWidget)
+        self.horizontalLayout.setObjectName("horizontalLayout")
+        self.label = QtWidgets.QLabel(TableIndexWidget)
+        self.label.setObjectName("label")
         self.horizontalLayout.addWidget(self.label)
-        self.comboBox = QtGui.QComboBox(TableIndexWidget)
-        self.comboBox.setObjectName(_fromUtf8("comboBox"))
+        self.comboBox = QtWidgets.QComboBox(TableIndexWidget)
+        self.comboBox.setObjectName("comboBox")
         self.horizontalLayout.addWidget(self.comboBox)
 
         self.retranslateUi(TableIndexWidget)
         QtCore.QMetaObject.connectSlotsByName(TableIndexWidget)
 
     def retranslateUi(self, TableIndexWidget):
-        TableIndexWidget.setWindowTitle(_translate("TableIndexWidget", "Form", None))
-        self.label.setText(_translate("TableIndexWidget", "TextLabel", None))
+        _translate = QtCore.QCoreApplication.translate
+        TableIndexWidget.setWindowTitle(_translate("TableIndexWidget", "Form"))
+        self.label.setText(_translate("TableIndexWidget", "TextLabel"))
 
